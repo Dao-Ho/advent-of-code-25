d62a49bf09fd2caa54a70a92f8a3a9d226773024
Dao Ho <84757503+Dao-Ho@users.noreply.github.com>
2025-12-03 01:53:52 -0500

    chore: day02 init

---
+++ day02/day02.go
@@ -0,0 +1,5 @@
+package day02
+
+func Part1(input string) int {
+	return 0
+}
---
+++ day02/day02_test.go
@@ -0,0 +1,21 @@
+package day02
+
+import (
+	"os"
+	"testing"
+)
+
+func TestPart1Example(t *testing.T) {
+	example, _ := os.ReadFile("example.txt")
+	got := Part1(string(example))
+	want := 3
+	if got != want {
+		t.Errorf("Part1() = %v, want %v", got, want)
+	}
+}
+
+func TestPart1(t *testing.T) {
+	input, _ := os.ReadFile("input.txt")
+	result := Part1(string(input))
+	t.Logf("Part 1: %d", result)
+}
